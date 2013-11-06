092ccb67cacd5c8535ed237fccc500542705bc54
Jakob Klinger <jklinge3@slu.edu>
2013-11-05 18:54:56 -0600

    Added the camera functionality and ablility to call albumfragment on double click

--- PhotoViewer/src/com/example/photoviewer/MainActivity.java
+++ PhotoViewer/src/com/example/photoviewer/MainActivity.java
@@ -1,39 +1,91 @@
 package com.example.photoviewer;
 
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
-public class MainActivity extends FragmentActivity implements AlbumFragment.ViewChangedListener {
+public class MainActivity extends SingleFragmentActivity implements AlbumFragment.ViewChangedListener{
 
-	GestureDetectorCompat gestureDetector;
+	GestureDetector gestureDetector;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+      //  hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+      //  hide the status bar and other OS-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_main);
+		gestureDetector = new GestureDetector(this, new GestureListener());
+		super.onCreate(savedInstanceState);
+		//FragmentManager manager = getSupportFragmentManager();
+		//Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+        
+    }
+	  @Override
+	    protected Fragment createFragment() {
+	            
+	        return new ViewFinderFragment();
+	    }
 	
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        gestureDetector = new GestureDetectorCompat(this, new GestureListener());
-        //view.startActivity(getIntent());
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	
+	}
+	
+    public boolean onTouchEvent(MotionEvent e) {
+        return gestureDetector.onTouchEvent(e);
     }
 	
 	class GestureListener extends GestureDetector.SimpleOnGestureListener {
-		
-		AlbumFragment albumFragment = (AlbumFragment) getSupportFragmentManager().findFragmentById(R.id.albumFragment);
-//		ImageFragment imageFragment = (ImageFragment) getSupportFragmentManager().findFragmentById(R.id.imageFragment);
-//		ViewFinderFragment vfFragment = (ViewFinderFragment) getSupportFragmentManager().findFragmentById(R.id.viewFinderFragment);
-		
-		//gesture stuff
+		   @Override
+	        public boolean onDown(MotionEvent e) {
+	            return true;
+	        }
+	        
+	        @Override
+	        public void onLongPress(MotionEvent e) {
+	            float x = e.getX();
+	            float y = e.getY();
+	            Log.d("Long Tap", "Tapped at: (" + x + "," + y + ")");
+	                
+	        }
+	        // event when double tap occurs
+	        @Override
+	        public boolean onDoubleTap(MotionEvent e) {
+	                
+	                FragmentManager manager = getSupportFragmentManager();
+	                manager.beginTransaction().replace(R.id.fragmentContainer, new AlbumFragment()).commit();
+	                float x = e.getX();
+	            float y = e.getY();
+	            Log.d("Double Tap", "Tapped at: (" + x + "," + y + ")");
+
+	            return true;
+	        }
+	
 	}
 
 	@Override
 	public void viewChanged(Image image) {
 		// TODO Auto-generated method stub
+		
 	}
+
+
 	
 }
--- PhotoViewer/src/com/example/photoviewer/ViewFinderFragment.java
+++ PhotoViewer/src/com/example/photoviewer/ViewFinderFragment.java
@@ -1,9 +1,14 @@
 package com.example.photoviewer;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.Size;
 import android.os.Build;
@@ -23,18 +28,66 @@ public class ViewFinderFragment extends Fragment {
 
     private Camera mCamera;
     private SurfaceView mSurfaceView;
+    private View mProgressContainer;
+    public static final String EXTRA_PHOTO_FILENAME = "CameraFragment.filename";
+    private Camera.ShutterCallback mShutterCallback = new Camera.ShutterCallback() {
+        public void onShutter() {
+            // display the progress indicator
+            mProgressContainer.setVisibility(View.VISIBLE);
+        }
+    };
+    private Camera.PictureCallback mJpegCallBack = new Camera.PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            // create a filename
+            String filename = UUID.randomUUID().toString() + ".jpg";
+            // save the jpeg data to disk
+            FileOutputStream os = null;
+            boolean success = true;
+            try {
+                os = getActivity().openFileOutput(filename, Context.MODE_PRIVATE);
+                os.write(data);
+            } catch (Exception e) {
+                Log.e(TAG, "Error writing to file " + filename, e);
+                success = false;
+            } finally {
+                try {
+                    if (os != null)
+                        os.close();
+                } catch (Exception e) {
+                    Log.e(TAG, "Error closing file " + filename, e);
+                    success = false;
+                } 
+            }
+            
+            if (success) {
+                // set the photo filename on the result intent
+                if (success) {
+                    Intent i = new Intent();
+                    i.putExtra(EXTRA_PHOTO_FILENAME, filename);
+                    getActivity().setResult(Activity.RESULT_OK, i);
+                } else {
+                    getActivity().setResult(Activity.RESULT_CANCELED);
+                }
+            }
+            getActivity().finish();
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent,
             Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_view_finder, parent, false);
 
-        Button takePictureButton = (Button) v.findViewById(R.id.view_finder_snapPicButton);
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                getActivity().finish();
-            }
-        });
+        mProgressContainer = v.findViewById(R.id.view_finder_progressContainer);
+        mProgressContainer.setVisibility(View.INVISIBLE);
+        
+        
+//        Button takePictureButton = (Button) v.findViewById(R.id.view_finder_snapPicButton);
+//        takePictureButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                getActivity().finish();
+//            }
+//        });
 
         mSurfaceView = (SurfaceView)v.findViewById(R.id.view_finder_surfaceView);
 
